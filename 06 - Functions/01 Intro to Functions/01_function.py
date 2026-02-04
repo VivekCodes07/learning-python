504f8968d6fdad1cd84f1ae70766da8bd6416625
Vivek Kr <vivekkrdas03@gmail.com>
2026-02-04 23:43:07 +0530

    Learned how to write functions

---
+++ 06 - Functions/01 Intro to Functions/01_function.py
@@ -0,0 +1,10 @@
+# Basic Function Syntax
+
+def add(a, b):
+    return a + b
+
+num1 = int(input("Enter number 1: "))
+num2 = int(input("Enter number 2: "))
+
+sum_value = add(num1, num2)
+print(f"Sum: {sum_value}")
