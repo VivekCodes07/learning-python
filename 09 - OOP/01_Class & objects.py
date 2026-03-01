d7ea84ef1e090cf932c0ab7810ad9e11f422006a
Vivek Kr <vivekkrdas03@gmail.com>
2026-03-01 16:32:47 +0530

    created my first class and objects

---
+++ 09 - OOP/01_Class & objects.py
@@ -0,0 +1,23 @@
+# blueprint
+class Car:
+    color = "red" # Attributes
+    brand = "bmw"
+    speed = 200
+
+# object 1
+car1 = Car()
+print(car1.color)
+
+# object 2
+car2 = Car()
+print(car2.brand)
+
+
+class Student:
+    name = "Vivek"
+
+s1 = Student()
+print(s1.name)
+
+s2 = Student()
+print(s2.name)
